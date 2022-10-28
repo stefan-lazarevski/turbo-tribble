1f64828605267d97d2233a4928994136abc2a222
stefan-lazarevski <116564446+stefan-lazarevski@users.noreply.github.com>
2022-10-28 17:11:02 +0200

    Create znachajnacifra.c

---
+++ znachajnacifra.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int broj;
+    scanf("%d", &broj);
+    printf("Najznachajna cifra e %d\n", broj/100);
+    printf("Najmalku znachajna cifra e %d\n", broj%10);
+    printf("%d",(broj/100+broj%10)>((broj/10)%10)*2);
+
+
+    return 0;
+}
