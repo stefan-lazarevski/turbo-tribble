0c62e1fae24093b5e059414357ecd4efde062846
stefan-lazarevski <116564446+stefan-lazarevski@users.noreply.github.com>
2022-10-30 19:00:58 +0100

    Update vezhba8.c

--- vezhba8.c
+++ vezhba8.c
@@ -23,3 +23,36 @@ int main()
 
     return 0;
 }
+
+/* Со додавање на знаците - и +*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    int poeni, ocena = 0;
+    printf("Vnesi poeni: \n");
+    scanf("%d", &poeni);
+    if (poeni < 0 || poeni > 100)
+        printf("Nevalidna vrednost za poeni! \n");
+    else {
+        if (poeni > 90) ocena = 10;
+    else if (poeni > 80) ocena = 9;
+    else if (poeni > 70) ocena = 8;
+    else if (poeni > 60) ocena = 7;
+    else if (poeni > 50) ocena = 6;
+    else ocena = 5;
+        char znak = ' ';
+        int pc = poeni & 10;
+        if (ocena != 5) {
+            if (pc >= 1 && pc <= 3) znak = '-';
+            else if (ocena !=10 && (pc >= 8 || pc == 0))
+                znak = '+';
+        }
+        printf("Ocenka %d%c\n", ocena , znak);
+    }
+
+    return 0;
+}
+
